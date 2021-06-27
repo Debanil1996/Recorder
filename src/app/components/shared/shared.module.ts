7bf35e37da01182f29759663e411fc322bd5c4f5
debanil.guha <debanil.guha@triquetra.in>
2021-06-27 09:53:59 +0530

    Added some features

---
+++ src/app/components/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { AppModule } from './../../app.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NgbModule,
+    AppModule
+  ],
+  entryComponents:[],
+  exports:[],
+  providers:[]
+})
+export class SharedModule { }
