0cc75b92fe96d42383cb92ea96b0370e347d2cf1
Debanil Guha <debanilguha@Debanils-MacBook-Pro.local>
2021-07-10 23:29:30 +0530

    Added for macosx

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import {NgModule} from '@angular/core';
 import {A11yModule} from '@angular/cdk/a11y';
-import {ClipboardModule} from '@angular/cdk/clipboard';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import {PortalModule} from '@angular/cdk/portal';
 import {ScrollingModule} from '@angular/cdk/scrolling';
@@ -47,7 +46,6 @@ import {OverlayModule} from '@angular/cdk/overlay';
 @NgModule({
   exports: [
     A11yModule,
-    ClipboardModule,
     CdkStepperModule,
     CdkTableModule,
     CdkTreeModule,
